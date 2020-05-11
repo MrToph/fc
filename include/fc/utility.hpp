5c5c30e5506f8029d2715290ee73fa453c960bc1
MrToph <christoph.michel.1992@gmail.com>
2020-05-11 11:47:47 +0400

    Increase MAX_NUM_ARRAY_ELEMENTS because we want to return a bigger payload in push_transaction

--- include/fc/utility.hpp
+++ include/fc/utility.hpp
@@ -15,8 +15,9 @@
 #define NO_RETURN __attribute__((noreturn))
 #endif
 
-#define MAX_NUM_ARRAY_ELEMENTS (1024*1024)
-#define MAX_SIZE_OF_BYTE_ARRAYS (20*1024*1024)
+// @cmichel changed to 10x because of HTTP payload return value
+#define MAX_NUM_ARRAY_ELEMENTS (10*1024*1024)
+#define MAX_SIZE_OF_BYTE_ARRAYS (20*10*1024*1024)
 
 //namespace std {
 //  typedef decltype(sizeof(int)) size_t;
